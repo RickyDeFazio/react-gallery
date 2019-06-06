c75c3ecd09de58b5076a65b2dbf1ba023875d76a
Ricky DeFazio <rickysdefazio@gmail.com>
2019-06-06 16:46:25 -0400

    Update

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const GalleryItem = ({ server, id, secret} ) => {
+const GalleryItem = ({ server, id, secret }) => {
   return (
     <li>
       <img alt="" src={`https://live.staticflickr.com/${server}/${id}_${secret}.jpg`}/>
